7d041af86e609b99f180a7491a276eaa4b0ff3e1
aduvva <126991278+aduvva@users.noreply.github.com>
2023-03-05 20:54:12 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents new Healthy Diner')
+streamlit.title('北 My Mom's new Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
