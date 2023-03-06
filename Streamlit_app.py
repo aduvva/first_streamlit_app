c51a409edd71626af5503a43618fa721aa308790
aduvva <126991278+aduvva@users.noreply.github.com>
2023-03-05 21:09:26 -0500

    Update Streamlit_app.py
    Removed the emoji syntax on line 5

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Moms new Healthy Diner')
 
-streamlit.header('\N{Food bowl} Breakfast Favorites \N{Food bowl}')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
