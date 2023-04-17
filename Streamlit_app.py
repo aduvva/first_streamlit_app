dfa4bdb448147413bba2c956475fbd2d1309d42a
aduvva <126991278+aduvva@users.noreply.github.com>
2023-04-17 08:28:54 +0530

    Update Streamlit_app.py
    add code in line 47 and 48 to add jackfruit to the list

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -43,3 +43,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruist load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#Allow the enduser to adda fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
