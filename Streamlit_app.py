63c708b6fdf12118eee0f55edddcbb8d3cdf349f
aduvva <126991278+aduvva@users.noreply.github.com>
2023-04-18 09:15:58 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -54,7 +54,7 @@ streamlit.stop()
 
 streamlit.header("The fruist load list contains:")
 #snowflake-related functions
-def get_frit_load_list():
+def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from fruit_load_list")
          return my_cur.fetchall()
